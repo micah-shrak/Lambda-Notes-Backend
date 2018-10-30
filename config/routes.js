b8c4307435e42ba4024ffc81bb4626d3146251dc
mich-shrak <micah.shrak@gmail.com>
2018-10-30 15:13:55 -0700

    Created end points

--- config/routes.js
+++ config/routes.js
@@ -1,22 +1,75 @@
+// axios
+//const axios = require('axios');
+
 // middleware
+
 // bcyrptjs
+
 // db
+const db = require('../database/dbConfig.js');
 
 module.exports = (server) => {
 	server.get('/', sanityCheck);
-	server.post('/api/register', register);
-	server.post('/api/login', login);
-	server.get('/api/users', listUsers);
 	server.get('/api/notes', listNotes);
-	server.get('/api/notes/:id', userNotes);
 	server.post('/api/notes', createNotes);
-	server.put('/api/note/:id', editNotes);
-	server.delete('/api/note/:id', deleteNotes);
+	server.put('/api/notes/:id', editNotes);
+	server.delete('/api/notes/:id', deleteNotes);
 };
 
 // Sanity Check
 function sanityCheck(req, res) {
 	res.send('Is this thing on???');
+}
+
+// LIST ALL NOTES
+function listNotes(req, res) {
+	db('notes')
+		.then((note) => {
+			res.status(200).json(note);
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+}
+
+// CREATE NOTES
+function createNotes(req, res) {
+	const note = req.body;
+	db.insert(note)
+		.into('notes')
+		.then((ids) => {
+			res.status(201).json(ids);
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+}
+
+// EDIT NOTES
+function editNotes(req, res) {
+	const { id } = req.params;
+	const changes = req.body;
+	db('notes')
+		.where({ id })
+		.update(changes)
+		.then((change) => {
+			res.status(200).json(change);
+		})
+		.catch((err) => res.status(500).json(err));
+}
+
+// DELETE NOTES
+function deleteNotes(req, res) {
+	const { id } = req.params;
+	db('notes')
+		.where({ id })
+		.del()
+		.then((count) => {
+			res.status(200).json(count);
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
 }
 
 // REGISTER
@@ -27,17 +80,5 @@ function login(req, res) {}
 // LIST ALL USERS
 function listUsers(req, res) {}
 
-// LIST ALL NOTES
-function listNotes(req, res) {}
-
 // LIST NOTES BY USER ID
 function userNotes(req, res) {}
-
-// CREATE NOTES
-function createNotes(req, res) {}
-
-// EDIT NOTES
-function editNotes(req, res) {}
-
-// DELETE NOTES
-function deleteNotes(req, res) {}
